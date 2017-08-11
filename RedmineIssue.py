bac2ad9b7d62e9aeb5d52024c1dd5d99457f58ba
--replace-all <nathanbowness@gmail.com>
2017-08-11 11:42:02 -0400

    Update to only one message response for Redmine.

--- RedmineIssue.py
+++ RedmineIssue.py
@@ -5,9 +5,7 @@ class Issue:
         **Can be extended
         :param issue_dict: dictionary with all information about the redmine issue being processed
         """
-        self.in_progress_msg = ""
-        self.success_msg = ""
-        self.error_msg = ""
+        self.redmine_msg = ""
 
         self.subject = issue_dict['subject']
         self.id = issue_dict['id']
